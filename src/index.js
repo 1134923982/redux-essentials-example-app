6277943f8620920a06d27ba1624d36a8624ff226
Jayden Zhang <jayden.zhang@oocl.com>
2021-04-08 14:35:04 +0800

    Add Redux guide project

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import App from './App'
 import store from './app/store'
 import { Provider } from 'react-redux'
 
+import { fetchUsers } from './features/users/usersSlice'
+
 import './api/server'
 
+store.dispatch(fetchUsers())
+
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 )
